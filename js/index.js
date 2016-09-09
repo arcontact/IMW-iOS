2d68c1d4c99114f3a36e344162da465229fb5cf7
arcontact <kamil@arcontact.pl>
2016-09-09 14:28:15 +0200

    hide pagination

--- js/index.js
+++ js/index.js
@@ -956,6 +956,10 @@ var	warsztaty = [],
 					warsztaty_from_file = true;
 				}
 			}
+			$(document).on("pageshow","#page1",function(){
+				$(".articles_pagination_outer").stop().fadeOut(100);
+				$(".warsztaty_pagination_outer").stop().fadeOut(100);
+			});
 			$(document).on("pagebeforeshow","#page2",function(){
 				if(articles_first_load){
 					articles_first_load = false;
